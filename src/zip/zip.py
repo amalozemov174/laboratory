78f7a858813e6ab54e8b0220fcd358b74d72c5c9
Anatoly <glenglen174@yandex.ru>
2026-08-16 15:55:11 +0500

    lesson trackers

--- src/zip/zip.py
+++ src/zip/zip.py
@@ -5,8 +5,8 @@
 #Удаление фалов
 #В общем решено верно,но не использую первую функцию из задачи
 #Напишите функцию, которая получает на вход два параметра: имя файла архива и расширение файла, сканирует текущий каталог в поисках файлов с подходящим расширением, и добавляет их в архив (исходно этот архив не существует).	
-import os.path
-from zipfile import ZipFile
+#import os.path
+#from zipfile import ZipFile
 
 def add_arch(arch_name, files_extension):
     if os.path.isfile(arch_name + '.zip'):
