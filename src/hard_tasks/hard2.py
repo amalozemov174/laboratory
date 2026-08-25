3fd484bfd42689d772b3615a23d4e82d28831cdf
Anatoly <glenglen174@yandex.ru>
2026-08-26 00:24:46 +0500

    hard 2

---
+++ src/hard_tasks/hard2.py
@@ -0,0 +1,7 @@
+def odometer(oksana):
+    length = 0
+    prev_time = 0
+    for i in range(0, len(oksana) - 1, 2):
+        length += oksana[i] * ( oksana[i + 1] - prev_time)
+        prev_time = oksana[i + 1]
+    return length 
